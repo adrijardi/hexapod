0374eb05c3c76917d90a9c97f4f8113957edd907
Adrian Lopez <adrian@coding42.com>
2020-12-01 19:38:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Code to support my hexapod robot
---
+++ leds.py
@@ -0,0 +1,44 @@
+import time
+from rpi_ws281x import *
+
+
+class LED:
+    def __init__(self):
+        self.LED_COUNT = 16  # SettothetotalnumberofLEDlightsontherobotproduct.There are more LED lightsontheRaspberryPi
+        self.LED_PIN = 12  # SettotheinputpinnumberoftheLEDgroup
+        self.LED_FREQ_HZ=800000
+        self.LED_DMA = 10
+        self.LED_BRIGHTNESS = 255
+        self.LED_INVERT = False
+        self.LED_CHANNEL = 0  # Usetheconfigurationitemabovetocreateastrip
+        self.strip = Adafruit_NeoPixel(
+            self.LED_COUNT,
+            self.LED_PIN,
+            self.LED_FREQ_HZ,
+            self.LED_DMA,
+            self.LED_INVERT,
+            self.LED_BRIGHTNESS,
+            self.LED_CHANNEL
+        )
+        self.strip.begin()
+
+
+    def colorWipe(self, R, G, B):  # ThisfunctionisusedtochangethecoloroftheLED light
+        color = Color(R, G, B)
+        for i in range(self.strip.numPixels()):  # OnlyoneLEDlightcolorcanbesetatatime,soweneedtodoaloop
+            self.strip.setPixelColor(i, color)
+            self.strip.show()  # Thecolorwillonlychangeaftercallingtheshowmethod
+
+
+if __name__ == '__main__':
+    LED = LED()
+    try:
+        while 1:
+            LED.colorWipe(255, 0, 0)  # Allthelightsturnred
+            time.sleep(1)
+            LED.colorWipe(0, 255, 0)  # Alllightsturngreen
+            time.sleep(1)
+            LED.colorWipe(0, 0, 255)  # Alllightsturnblue
+            time.sleep(1)
+    except:
+        LED.colorWipe(0, 0, 0)  # Turnoffalllights
